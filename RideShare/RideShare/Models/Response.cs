f4b04cf5c32f85d4e06d127ae3606302b5e1cefe
rizlajf <rizla.jf@gmail.com>
2016-07-18 14:41:21 +0530

    Added missing file

---
+++ RideShare/RideShare/Models/Response.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideShare.Models
+{
+    public class UserJson
+    {       
+        public List<Response> users { get; set; }
+    }
+
+    public class Response : JsonResponse
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        [JsonProperty("FirstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("LastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("UserName")]
+        public string UserName { get; set; }
+
+        [JsonProperty("Email")]
+        public string Email { get; set; }
+
+        [JsonProperty("userType")]
+        public string UserType { get; set; }
+
+        [JsonProperty("Longitude")]
+        public string Longitude { get; set; }
+
+        [JsonProperty("Latitude")]
+        public string Latitude { get; set; }
+
+    }
+}
